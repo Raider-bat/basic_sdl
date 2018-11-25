dff4130033fe7b80f6efc9f0c876fca2693c8d2a
lindevhard <lindevhard@gmail.com>
2018-11-25 17:27:35 +0600

    init of FPSManager

---
+++ code/src/FPSManager.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CUtils"
+#include <chrono>
+#include <thread>
+
+class FPSManager()
+{
+public:
+  void LimitFPS(float _fps);
+  void ShowCurrentFPS();
+  float GetCurrentFPS();
+  float GetLimitFPS();
+
+private:
+
+  float currentFps; //value of current fps
+  float targetFps;
+
+  static const float MIN_FPS (15.0);
+  static const float MAX_FPS (60.0);
+};
