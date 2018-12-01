0bf4bd65f6267770484a4f700214c44fa42369b4
raider-bat <valeraaitpekov@gmail.com>
2018-12-02 03:24:50 +0600

    update all

---
+++ build/code/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/raider-bat/basic_sdl/code/src/AbstractWindow.cpp" "/home/raider-bat/basic_sdl/build/code/CMakeFiles/code.dir/src/AbstractWindow.cpp.o"
+  "/home/raider-bat/basic_sdl/code/src/Utils.cpp" "/home/raider-bat/basic_sdl/build/code/CMakeFiles/code.dir/src/Utils.cpp.o"
+  "/home/raider-bat/basic_sdl/code/src/main.cpp" "/home/raider-bat/basic_sdl/build/code/CMakeFiles/code.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/SDL2"
+  "../&{GLM_INCLUDE_DIRS}"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/code/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/src/main.cpp.o"
+  "CMakeFiles/code.dir/src/AbstractWindow.cpp.o"
+  "CMakeFiles/code.dir/src/Utils.cpp.o"
+  "code.pdb"
+  "code"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
