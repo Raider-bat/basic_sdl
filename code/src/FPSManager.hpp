8bf537f2592c3039174d602404a6f2bb0c957580
raider-bat <valeraaitpekov@gmail.com>
2018-12-02 03:32:49 +0600

    Fix FPSManager.hpp

--- code/src/FPSManager.hpp
+++ code/src/FPSManager.hpp
@@ -1,27 +1,21 @@
 #pragma once
 
-#include "Utils.hpp"
+#include "CUtils"
 #include <chrono>
 #include <thread>
 
-class FPSManager
+class FPSManager()
 {
 public:
-  FPSManager();
-
-  void LimitFPS(const std::chrono::milliseconds &framePeriod);
-  float GrabDeltaTime();
-  void UpdateCurrentFPS();
-  void ShowCurrentFPS() const;
-  void SetTargetFPS();
-  float GetCurrentFPS() const;
-  float GetTargetFPS() const;
+  void LimitFPS(float _fps);
+  void ShowCurrentFPS();
+  float GetCurrentFPS();
+  float GetTargetFPS();
 
 private:
   float currentFps; //value of current fps
   float targetFps;
-  std::chrono::time_point<std::chrono::system_clock> m_lastTime;
-  const  float MIN_FPS = 15.0;
-  const  float MAX_FPS = 60.0;
-  //static const float;
+
+  static const float MIN_FPS (15.0);
+  static const float MAX_FPS (60.0);
 };
